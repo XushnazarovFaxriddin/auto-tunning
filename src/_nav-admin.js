7868a22a0411e7c7812464bae4313f73ba35e26a
Faxriddin Xushnazarov <coderxushnazarov@gmail.com>
2024-09-28 21:01:25 +0500

    first commit

---
+++ src/_nav-admin.js
@@ -0,0 +1,27 @@
+export default [
+    {
+      component: 'CNavItem',
+      name: 'Dashboard',
+      to: '/admin/dashboard',
+      icon: 'cil-speedometer',
+    },
+    {
+        component: 'CNavItem',
+        name: 'Orders',
+        to: '/admin/orders',
+        icon: 'cil-notes',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Workers',
+        to: '/admin/workers',
+        icon: 'cil-people',
+      },
+      {
+        component: 'CNavItem',
+        name: 'Reception',
+        to: '/admin/reception',
+        icon: 'cil-user',
+      }
+  ]
+  
---
+++ src/components/AppHeaderDropdownAccnt.vue
@@ -0,0 +1,27 @@
+<script setup>
+import avatar from '@/assets/images/avatars/2.jpg'
+
+const itemsCount = 42
+function isLogout() {
+  window.location.href = "/"
+}
+</script>
+
+<template>
+  <CDropdown placement="bottom-end" variant="nav-item">
+    <CDropdownToggle class="py-0 pe-0" :caret="false">
+      <CAvatar :src="avatar" size="md" />
+    </CDropdownToggle>
+    <CDropdownMenu class="pt-0" style="z-index: 1022;">
+      <CDropdownHeader
+        component="h6"
+        class="bg-body-secondary text-body-secondary fw-semibold mb-2 rounded-top"
+      >
+        Account
+      </CDropdownHeader>
+      <CDropdownItem style="cursor: pointer;"> <CIcon icon="cil-user" /> Profile </CDropdownItem>
+      <CDropdownItem style="cursor: pointer;" @click="isLogout"> <CIcon icon="cil-lock-locked" /> Logout </CDropdownItem>
+    </CDropdownMenu>
+  </CDropdown>
+</template>
+
